bebe90de22f39532177aedab91aede5d53ff8d54
María Mira <maria.alaves93@gmail.com>
2024-10-21 11:36:28 +0200

    Realizadas tareas de Fundamentos_js_II. Añadidas y modificadas funciones y objetos. Todos los test pasados

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,8 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(cantidad) {
   if (cantidad >= 0) {
@@ -11,13 +13,13 @@ function actualizarPresupuesto(cantidad) {
     return -1;
   };
 
-}
+};
 
 function mostrarPresupuesto() {
   return `Tu presupuesto actual es de ${presupuesto} €`;
-}
+};
 
-function CrearGasto(descripcion, gasto) {
+function CrearGasto(descripcion, gasto, fecha = Date.now(), ...etiquetas) {
   this.descripcion = descripcion;
 
   if (gasto >= 0) {
@@ -26,8 +28,30 @@ function CrearGasto(descripcion, gasto) {
     this.valor = 0;
   }
 
+  if (isNaN(Date.parse(fecha))) {
+    this.fecha = Date.now();
+  } else {
+    this.fecha = Date.parse(fecha);
+  }
+
+  if (etiquetas.length == 0) {
+    this.etiquetas = [];
+  } else {
+    this.etiquetas = etiquetas
+  }
+
   this.mostrarGasto = function () {
     return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+  }
+
+  this.mostrarGastoCompleto = function () {
+    let fechaFormatoLocalizado = new Date(this.fecha).toLocaleString();
+
+    let etiquetasFormateadas = this.etiquetas.map(etiqueta => `- ${etiqueta}`).join('\n');
+
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\n` +
+      `Fecha: ${fechaFormatoLocalizado}\n` +
+      `Etiquetas:\n${etiquetasFormateadas}\n`
   }
 
   this.actualizarDescripcion = function (nuevaDescripcion) {
@@ -40,7 +64,50 @@ function CrearGasto(descripcion, gasto) {
     }
   }
 
+  this.actualizarFecha = function (fechaNueva) {
+    if (!isNaN(Date.parse(fechaNueva))) {
+      this.fecha = Date.parse(fechaNueva);
+    }
+  }
+
+  this.anyadirEtiquetas = function (...etiquetasNuevas) {
+    etiquetasNuevas.forEach(etiqueta => {
+      if (!this.etiquetas.includes(etiqueta)) {
+        this.etiquetas.push(etiqueta);
+      }
+    })
+  }
+
+  this.borrarEtiquetas = function (...etiquetasParaBorrar) {
+    this.etiquetas = this.etiquetas.filter(etiqueta => !etiquetasParaBorrar.includes(etiqueta));
+  }
+
 }
+
+function listarGastos() {
+  return gastos;
+};
+
+function anyadirGasto(gasto) {
+  gasto.id = idGasto;
+  idGasto++;
+  gastos.push(gasto);
+};
+
+function borrarGasto(id) {
+  gastos = gastos.filter(gasto => gasto.id != id);
+};
+
+function calcularTotalGastos() {
+  return gastos.reduce((suma, gasto) => suma + gasto.valor, 0);
+};
+
+function calcularBalance() {
+  let gastosTotales = calcularTotalGastos();
+
+  return presupuesto - gastosTotales;
+};
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -48,5 +115,10 @@ function CrearGasto(descripcion, gasto) {
 export {
   mostrarPresupuesto,
   actualizarPresupuesto,
-  CrearGasto
+  CrearGasto,
+  listarGastos,
+  anyadirGasto,
+  borrarGasto,
+  calcularTotalGastos,
+  calcularBalance
 }
