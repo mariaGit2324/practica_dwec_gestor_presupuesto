31eed500bf3adab6d54cf6867c382c42dd6da0c1
María Mira <maria.alaves93@gmail.com>
2024-10-21 09:20:22 +0200

    Realizadas tareas de Fundamentos_js_I. Creada variable global y funciones correspondientes en gestionPresupuesto.js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,53 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+let presupuesto = 0;
 
+function actualizarPresupuesto(cantidad) {
+  if (cantidad >= 0) {
+    presupuesto = cantidad;
+    return presupuesto;
+  } else {
+    console.log("Se ha introducido una cantidad negativa.");
+    return -1;
+  };
 
-function actualizarPresupuesto() {
-    // TODO
 }
 
 function mostrarPresupuesto() {
-    // TODO
+  return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, gasto) {
+  this.descripcion = descripcion;
+
+  if (gasto >= 0) {
+    this.valor = gasto;
+  } else {
+    this.valor = 0;
+  }
+
+  this.mostrarGasto = function () {
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+  }
+
+  this.actualizarDescripcion = function (nuevaDescripcion) {
+    this.descripcion = nuevaDescripcion;
+  }
+
+  this.actualizarValor = function (nuevoGasto) {
+    if (nuevoGasto >= 0) {
+      this.valor = nuevoGasto;
+    }
+  }
+
+}
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
+export {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto
 }
