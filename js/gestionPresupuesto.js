31810fc2076753095cd0c6804879beeaf2920585
María Mira <maria.alaves93@gmail.com>
2025-01-26 19:34:21 +0100

    Realizada y exportada función transformarListadoEtiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -186,6 +186,10 @@ function agruparGastos(periodo = "mes", etiquetas, fechaDesde, fechaHasta) {
   }, {});
 };
 
+function transformarListadoEtiquetas(etiquetas) {
+  return etiquetas.match(/[a-z0-9]+/gi);
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -199,5 +203,6 @@ export {
   calcularTotalGastos,
   calcularBalance,
   filtrarGastos,
-  agruparGastos
+  agruparGastos,
+  transformarListadoEtiquetas
 }
