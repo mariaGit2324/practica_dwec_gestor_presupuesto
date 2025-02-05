cfdae9f2414e56bb676fab7d9f8cc9f22246245d
María Mira <maria.alaves93@gmail.com>
2025-02-05 21:42:48 +0100

    Añadida y exportada función cargarGastos en gestionPresupuesto.js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -190,6 +190,16 @@ function transformarListadoEtiquetas(etiquetas) {
   return etiquetas.match(/[a-z0-9]+/gi);
 }
 
+function cargarGastos(gastosAlmacenamiento) {
+  gastos = [];
+
+  for (let g of gastosAlmacenamiento) {
+    let gastoRehidratado = new CrearGasto();
+    Object.assign(gastoRehidratado, g);
+    gastos.push(gastoRehidratado)
+  }
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -204,5 +214,6 @@ export {
   calcularBalance,
   filtrarGastos,
   agruparGastos,
-  transformarListadoEtiquetas
+  transformarListadoEtiquetas,
+  cargarGastos
 }
